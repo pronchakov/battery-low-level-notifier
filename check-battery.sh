9266fcdc0c3c030f6350072e98dcc634206c25e5
Artem Pronchakov <artem.pronchakov@calisto.email>
2023-01-26 22:09:04 +0300

    fix threshold

--- check-battery.sh
+++ check-battery.sh
@@ -3,7 +3,7 @@
 battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ $battery_level -lt 90 ] && [ $battery_status != "Charging" ]; then
+if [ $battery_level -lt 10 ] && [ $battery_status != "Charging" ]; then
   export XDG_RUNTIME_DIR=/run/user/$(id -u)
   notify-send -u critical Battery "Battery charge level is critical."
 fi
